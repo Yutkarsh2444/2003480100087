b4c0d3c71fc274275940254a60f70e02c6ed7835
Yutkarsh2444 <98741927+Yutkarsh2444@users.noreply.github.com>
2023-10-06 12:53:58 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import NumberMerger from './NumberManagement';
+
+function App() {
+  return (
+    <div className="App">
+      <NumberMerger />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/chai.js
@@ -0,0 +1,8 @@
+function Chai()
+{
+    return (
+        <h2>Chai or coffee</h2>
+    )
+}
+
+export default Chai;
